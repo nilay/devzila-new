c4d5eb4bead7fc2033d13fbe2269df8a72950c6e
Nilay <nilayanand@gmail.com>
2015-06-14 20:38:25 +0530

    Add .htaccess and other pages

---
+++ about-us.php
@@ -0,0 +1,19 @@
+<?php
+$TITLE = "Devzila";
+include_once("include/header.php");
+?>
+    <div class="container">
+    <div style="margin-bottom: 40px;">
+    </div>
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-4 margin-bottom30">
+            <center>
+                <p>About Us</p>
+           </center>        
+        </div>
+      </div>
+<?php
+include_once("include/footer.php");
+?>
+
