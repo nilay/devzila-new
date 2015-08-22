4643cc34fddbef327b7bad47bf885ee0ddefb902
Nilay <nilayanand@gmail.com>
2015-08-22 20:37:30 +0530

    Add internal page

---
+++ include/_contact.php
@@ -0,0 +1,23 @@
+<div class="panel panel-default">
+  <div class="panel-body">
+    <h4>Let's Keep In Touch!</h4>
+    <form role="form">
+      <div class="form-group">
+        <label for="email">Name:</label>
+        <input type="email" class="form-control" id="email">
+      </div>
+      <div class="form-group">
+        <label for="pwd">Email:</label>
+        <input type="password" class="form-control" id="pwd">
+      </div>
+      <div class="form-group">
+        <label for="smt">What would you like to discuss?</label>
+        <textarea class="form-control"></textarea>  
+      </div>
+      <button type="submit" class="btn btn-default">Send</button>
+    </form> 
+            
+            
+  </div>
+</div>    
+            
